b7e98a84b8bb3e31e952917e793e292c103f7af0
206 <206@academicos.cesde.edu.co>
2022-11-30 17:33:30 -0500

    se crea proyecto

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //asociar objetos xml con java y eliminar barra de titulo por defecto
+        getSupportActionBar().hide();
+
+    }
+}
